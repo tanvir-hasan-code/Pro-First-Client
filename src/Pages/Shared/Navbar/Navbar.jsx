75844ba66b51d914d550013ae4f496b58162292c
tanvir-hadan-code <tanvirx015@gmail.com>
2025-09-18 19:43:26 +0600

    Theme Impiliment

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router";
 import ProFastLogo from "../ProFastLogo/ProFastLogo";
 
 const Navbar = () => {
+const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
 
-	const navItem = <>
-	<li><NavLink to="/">Home</NavLink></li>
-	<li><NavLink to="/about">About Us</NavLink></li>
-	</>
+  useEffect(() => {
+  document.documentElement.setAttribute("data-theme", theme); 
+  localStorage.setItem("theme", theme);
+}, [theme]);
 
+  const navItem = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/about">About Us</NavLink>
+      </li>
+    </>
+  );
 
   return (
     <div className="navbar bg-base-100 shadow-sm">
@@ -38,15 +49,39 @@ const Navbar = () => {
             {navItem}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl"><ProFastLogo/></a>
+        <a className="btn btn-ghost text-xl">
+          <ProFastLogo />
+        </a>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          {navItem}
-        </ul>
+        <ul className="menu menu-horizontal px-1">{navItem}</ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <label className="swap swap-rotate">
+          <input
+            type="checkbox"
+            onChange={() =>
+              setTheme(theme === "light" ? "dark" : "light")
+            }
+            checked={theme === "dark"}
+          />
+          {/* Dark Icon */}
+          <svg
+            className="swap-on fill-current w-6 h-6"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <path d="M21.64 13a9 9 0 11-9-9 7 7 0 009 9z"></path>
+          </svg>
+          {/* Light Icon */}
+          <svg
+            className="swap-off fill-current w-6 h-6"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <path d="M5.64 17.64a1 1 0 01-.7-1.7l2-2a1 1 0 011.41 1.41l-2 2a1 1 0 01-.71.29zm0-11.28a1 1 0 01.7-1.7l2 2a1 1 0 11-1.41 1.41l-2-2zm12.72 0l-2 2a1 1 0 01-1.41-1.41l2-2a1 1 0 011.41 1.41zm0 11.28a1 1 0 01-.7 1.7l-2-2a1 1 0 011.41-1.41l2 2a1 1 0 01.71.71z"></path>
+          </svg>
+        </label>
       </div>
     </div>
   );
